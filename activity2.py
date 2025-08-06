2022809d1dceef6a542df9283868db93eac9ed2e
emmett555505 <edonaldson1122@gmail.com>
2025-08-06 14:04:32 -0400

    lesson 17

---
+++ activity2.py
@@ -0,0 +1,18 @@
+import random
+
+options = ["rock", "paper", "scissors"]
+
+user_choice = input("choose rock paper or scissors : ")
+
+computer_choice = random.choice(options)
+
+if user_choice == computer_choice:
+    print("it's a tie")
+elif user_choice == "paper" and computer_choice == "rock":
+    print("paper covers rock you win!")
+elif user_choice == "rock" and computer_choice == "scissors":
+    print("rock smashes scissors you win!")
+elif user_choice == "scissors" and computer_choice == "paper":
+    print("scissors cut paper you win!")
+else:
+    print("you lose")
